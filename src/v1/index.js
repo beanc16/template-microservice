94cf1768b6227714f4459a7d5a9d3f68996a270a
beanc16 <bean.christopher@outlook.com>
2023-01-14 20:12:24 -0500

    remove commented out code

--- src/v1/index.js
+++ src/v1/index.js
@@ -20,21 +20,3 @@ export default app;
 import { pingEndpoints } from "./routes/index.js";
 
 app.use(`/ping`, pingEndpoints);
-
-
-
-
-
-/********************
- * ERROR MIDDLEWARE *
- ********************/
-
-/*
-app.use(function (err, req, res, next)
-{
-    // Error-handling middleware is defined last, after other app.use() and routes calls
-    console.error(err);
-
-    res.send(err);
-});
-*/
